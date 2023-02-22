74354e5e377ea69373b862c8736c129e82fbd668
markonedin <117040318+markonedin@users.noreply.github.com>
2023-02-22 23:09:55 +0100

    Updates

---
+++ precache-manifest.22dbcab23f74dd3dccfcf9f01d29602f.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "3701fe6798d106637d33",
+    "url": "/static/css/main.672823fd.chunk.css"
+  },
+  {
+    "revision": "3701fe6798d106637d33",
+    "url": "/static/js/main.3701fe67.chunk.js"
+  },
+  {
+    "revision": "f4bc99d3e1462336e0fd",
+    "url": "/static/js/1.f4bc99d3.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a666a093997e37f4e0b007ac77c657fc",
+    "url": "/static/media/bird.a666a093.svg"
+  },
+  {
+    "revision": "a4ecaf5fe64129bca06c5dbf2b350744",
+    "url": "/static/media/cat.a4ecaf5f.svg"
+  },
+  {
+    "revision": "d2066a8abe4a570f2e950cc249abd4d7",
+    "url": "/static/media/cow.d2066a8a.svg"
+  },
+  {
+    "revision": "52e6bfb7ce14438c484c518f045d0155",
+    "url": "/static/media/dog.52e6bfb7.svg"
+  },
+  {
+    "revision": "ead92468a9347e3c478ec0bf52e0c22d",
+    "url": "/static/media/gator.ead92468.svg"
+  },
+  {
+    "revision": "38abe518abe374db95433cdb03be84f8",
+    "url": "/static/media/heart.38abe518.svg"
+  },
+  {
+    "revision": "05a990adafcebd79b81d32d86215a89d",
+    "url": "/static/media/horse.05a990ad.svg"
+  },
+  {
+    "revision": "6be8392759d0362907caf603bb1c8e8c",
+    "url": "/index.html"
+  }
+];
